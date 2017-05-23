58fb0f23e94ba0f90c2d941ecee585d423e228f9
Mateus Schmitz <matteuschmitz@gmail.com>
2017-05-22 23:36:57 -0300

    versão beta do site: ok

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="PT-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta property="og:url" content="http://temerjarenunciou.com.br" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Temer Já Renunciou? NÃO" />
+        <meta property="og:description" content="Saiba se Michel Temer ainda é o presidente da República Federativa do Brasil" />
+        <meta property="og:image" content="http://temerjarenunciou.com.br/img/michel_temer.jpg" />
+        <meta property="og:locale" content="PT-br" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@temerrenunciou" />
+        <meta name="twitter:title" content="Temer Já Renunciou? NÃO" />
+        <meta name="twitter:description" content="Saiba se Michel Temer ainda é o presidente da República Federativa do Brasil" />
+        <meta name="twitter:image" content="http://temerjarenunciou.com.br/img/michel_temer.jpg" />
+
+        <title>Temer Já Renunciou?</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/custom-styles.css">
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-99746301-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+    </head>
+    <body>
+        <div class="jumbotron">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 mar-top-60">
+                        <h1 class="text-center">NÃO!</h1>
+                        <p class="text-center mar-top-30">Michel Temer segue como 37º presidente da República Federativa do Brasil, desde 31 de agosto de 2016.</p>
+                    </div>
+                    <img src="img/michel_temer.jpg" class="img-rounded img-responsive col-sm-4">
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row" style="margin-bottom: 60px">
+                <div class="col-sm-8">    
+                    <u><h2 class="text-center">ÚLTIMAS NOTÍCIAS</h2></u>
+                    <ul style="margin-top: 15px;">
+                    <li><a target="_blank" href="http://g1.globo.com/politica/noticia/temer-vai-a-residencia-de-maia-para-jantar-com-lideres-da-base-aliada.ghtml">
+                        Temer vai à residência de Maia para jantar com líderes da base aliada
+                    </a></li>
+                    <li><a target="_blank" href="http://g1.globo.com/politica/noticia/audio-gravado-por-delator-e-transmissao-original-da-cbn-tem-6-minutos-de-diferenca.ghtml">
+                        Áudio gravado por delator e transmissão original da CBN têm 6 minutos de diferença
+                    </a></li>
+                    <li><a target="_blank" href="http://g1.globo.com/politica/noticia/eventual-processo-de-impeachment-de-temer-pode-agravar-crise-diz-renan.ghtml">
+                        Eventual processo de impeachment de Temer 'pode agravar' crise, diz Renan
+                    </a></li>
+                    <li><a target="_blank" href="http://g1.globo.com/politica/noticia/em-situacao-normal-gravacao-nao-seria-aceita-diz-perito-contratado-pela-defesa-de-temer.ghtml">
+                        Gravação não seria aceita em 'situação normal', diz perito da defesa de Temer
+                    </a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <a class="twitter-timeline" href="https://twitter.com/temerrenunciou">Tweets by Temer Já Renunciou</a>
+                </div>
+            </div>
+        </div>
+
+        <footer class="footer">
+            <div class="container">
+                <p class="text-muted text-center">Todo o conteúdo aqui exposto pertence aos seus respectivos criadores e autores. Site melhor visualizado com o monitor ligado.</p>
+                <p class="text-muted text-center" style="margin-top: -20px;">Powered by <a target="_blank" href="http://temerjarenunciou.com.br">Temer Já Renunciou?</a></p>
+            </div>
+        </footer>
+    </body>
+    <script src="js/widgets.js" charset="utf-8"></script>
+</html>
